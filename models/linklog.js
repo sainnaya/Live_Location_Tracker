430a52fe945750cbd2aa75e43c72b4dc2d3569f0
sainnaya <nayasain9@gmail.com>
2026-07-17 11:48:48 +0530

    update

--- models/linklog.js
+++ models/linklog.js
@@ -1,22 +1,13 @@
 const mongoose = require("mongoose");
 
 const LinkLogSchema = new mongoose.Schema({
-
     token: String,
-
     ip: String,
-
     browser: String,
-
-    userId: String,
-
-    locationPermission: String,
-
     openedAt: {
         type: Date,
         default: Date.now
     }
-
 });
 
 module.exports = mongoose.model("LinkLog", LinkLogSchema);
